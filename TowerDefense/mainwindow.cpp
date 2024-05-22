74f88c878f3526f6ce075af4f1b8b19cf3a8daf0
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-22 13:51:13 +0200

    (feat) Added return to enu button on page game

--- TowerDefense/mainwindow.cpp
+++ TowerDefense/mainwindow.cpp
@@ -8,8 +8,9 @@ MainWindow::MainWindow(QWidget *parent)
     this->setWindowTitle("Tower Defense"); // Définir le titre de la fenêtre
     // this->setStyleSheet("background-image: url(:/assets/img/homeBackground.jpg); background-repeat: no-repeat; background-position: center;");
     connect(ui->buttonGame, &QPushButton::clicked, this, &MainWindow::showGamePage);
+    connect(ui->buttonMenu, &QPushButton::clicked, this, &MainWindow::showMenuPage);
 }
-
+// menuButton
 MainWindow::~MainWindow() {
     delete ui;
 }
@@ -17,6 +18,10 @@ MainWindow::~MainWindow() {
 void MainWindow::showGamePage()
 {
     ui->stackedWidget->setCurrentWidget(ui->pageGame);
-
 }
 
+void MainWindow::showMenuPage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->pageMenu);
+
+}
--- TowerDefense/mainwindow.h
+++ TowerDefense/mainwindow.h
@@ -15,6 +15,7 @@ public:
     ~MainWindow();
 
 private slots:
+    void showMenuPage();
     void showGamePage();
 
 private:
