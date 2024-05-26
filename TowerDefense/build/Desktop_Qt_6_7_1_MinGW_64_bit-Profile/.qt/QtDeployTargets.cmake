98c8499cc4aba19122e4af7a21f1c514aef8b790
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-26 17:38:38 +0200

    (feat) test merge qt

---
+++ TowerDefense/build/Desktop_Qt_6_7_1_MinGW_64_bit-Profile/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_TowerDefense_FILE C:/Users/lucas/Desktop/Qt/POODPUML/TowerDefense/build/Desktop_Qt_6_7_1_MinGW_64_bit-Profile/TowerDefense.exe)
+set(__QT_DEPLOY_TARGET_TowerDefense_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
